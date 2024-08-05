688fb6d5edc9db29367c1af69fff5803bb1cc5fe
ankurambastha <reaper.node@gmail.com>
2024-08-05 23:54:01 +0530

    day 3

--- Part2OOP/InheritanceAndTraits.scala
+++ Part2OOP/InheritanceAndTraits.scala
@@ -2,6 +2,7 @@ package Part2OOP
 
 object InheritanceAndTraits extends App {
 class Animal {
+  val creatureType = "Fog"
   protected def eat(): Unit = println("nomnom")
 
 }
@@ -15,4 +16,28 @@ class Animal {
   val cat = new Cat
   cat.crunch
 
+
+  class Person(name: String, age: Int)
+//  class Adult(name: String, age: Int) extends Person
+// here since person is the parent class with name and age as constructor value so above defination will not work
+  class Adult(name: String, age: Int) extends Person(name, age)
+  // we need to define name and age
+
+  // but if you define an auxiliary constructor
+
+  class NewPerson(name: String, age: Int){
+    def this(name: String) = this(name,0)
+    def this() = this("ankur",0)
+  }
+
+  class NewAdult(name: String, age: Int) extends NewPerson
+
+  class Dog extends Animal{
+    override vals creatureType = "Zog"
+    override def eat(): Unit = {
+      println("dic dic")
+    }
+  }
+
 }
+
--- PractiseProblems/roughPage.sc
+++ PractiseProblems/roughPage.sc
@@ -2,3 +2,10 @@ val s:String ="ICV"
 s.head
 s.tail.head
 s.tail.tails.toList
+
+
+val aList: List[Int] = List(2,3,4,5,6,7,7)
+
+val aMapList = aList.map(_ * 2)
+val aFlatMap = aList.flatMap(_a => List(_a*2))
+
